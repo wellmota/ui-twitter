25dcc7b181dc6d77692bb34128ebe12457781872
wellmota <impeto.wellington@gmail.com>
2024-03-27 18:16:12 -0300

    creating side bar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./global.css"
+import {
+  Bell,
+  BookmarkSimple,
+  DotsThreeCircle,
+  Envelope,
+  FileText,
+  Hash,
+  House,
+  User,
+} from "@phosphor-icons/react"
+import twitterLogo from "./assets/logo-twitter.svg"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div className="layout">
+      <aside className="sidebar">
+        <img className="logo" src={twitterLogo} alt="Logo" />
+        <nav className="main-navigation">
+          <a className="active" href="">
+            <House weight="fill" />
+            Home
+          </a>
+          <a href="">
+            <Hash />
+            Explore
+          </a>
+          <a href="">
+            <Bell />
+            Notifications
+          </a>
+          <a href="">
+            <Envelope />
+            Messages
+          </a>
+          <a href="">
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            Lists
+          </a>
+          <a href="">
+            <User />
+            Profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            More
+          </a>
+        </nav>
+        <button className="new-tweet" type="button">
+          Tweet
+        </button>
+      </aside>
+      <div className="content">content</div>
+    </div>
+  </React.StrictMode>
 )
